8bb5606ba0ac4034514b6102811e65cc6c145827
pruthvi_21 <pruthvishankar397@gmail.com>
2021-04-13 20:11:06 +0530

    Created home screen layout

--- app/src/main/java/com/android/har/MainActivity.kt
+++ app/src/main/java/com/android/har/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.android.har
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openFileViewer(view: View) {
+        //TODO: open file viewer to select a video
+    }
+
+    fun openCamera(view: View) {
+        //TODO: open camera activity
     }
 }
