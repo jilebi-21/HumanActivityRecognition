a44e15b913b9b8f85e1f43c82cb8925b2d42f304
pruthvi_21 <pruthvishankar397@gmail.com>
2021-04-18 17:49:54 +0530

    Added some basic settings

--- app/src/main/java/com/android/har/SettingsActivity.kt
+++ app/src/main/java/com/android/har/SettingsActivity.kt
@@ -1,11 +1,37 @@
 package com.android.har
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.settings_container, SettingsFragment.newInstance())
+            .commit()
+    }
+
+    class SettingsFragment private constructor() : PreferenceFragmentCompat() {
+
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            addPreferencesFromResource(R.xml.camera_settings)
+        }
+
+        companion object {
+            fun newInstance(): SettingsFragment {
+                return SettingsFragment()
+            }
+        }
+
     }
 }
